fe5d077c1c49e9f53f6924562f4cc251a20a23bb
c-carl <carlosvirla10@gmail.com>
2025-08-28 21:08:14 -0300

    Aula 01 de Python
    Códigos da minha primeira aula de Python

---
+++ aula python/python.py
@@ -0,0 +1,28 @@
+print("\n")
+print("="*66)
+name=input("Prezado usuário digite o seu nome: ")
+print("="*66)
+idade=int(input("Digite a sua idade: "))
+print("="*66)
+course=input("Digite o curso que você está fazendo: ")
+print("="*66)
+email=input("Digite o seu email: ")
+print("="*66)
+cell=input("Digite o seu celular com DDD: ")
+print("="*66)
+renda=float(input("Digite a sua renda: "))
+print("="*66)
+print("\n")
+
+print("="*66)
+print("                      Relatório de Dados")
+print("="*66)
+print("\n")
+print(f"Nome: {name}\nEmail: {email}\nTelefone: {cell}\nIdade: {idade}\nRenda R$: {renda:.3f}")
+print(" "*1000)
+
+
+
+
+
+
